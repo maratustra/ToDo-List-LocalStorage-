e1bfdec8fa7e3eb231b103528db1939d4202f67b
Alena Maramygina <maratustraa@gmail.com>
2021-10-19 13:34:03 +0500

    создан проект todo list 'use scrict';

---
+++ js/script.js
@@ -0,0 +1,62 @@
+'use scrict';
+
+const todoControl = document.querySelector('.todo-control');
+const headerInput = document.querySelector('.header-input');
+const todoList = document.querySelector('.todo-list');
+const todoCompleted = document.querySelector('.todo-completed');
+
+let todoData = [];
+
+const render = function () {
+  todoList.innerHTML = '';
+  todoCompleted.innerHTML = '';
+
+  todoData.forEach(function (item, itemIndex) {
+
+    const li = document.createElement('li');
+
+    li.classList.add("todo-item");
+
+    li.innerHTML = `<span class="text-todo">${item.text}</span>
+    <div class="todo-buttons">
+      <button class="todo-remove"></button>
+      <button class="todo-complete"></button>
+    </div>`;
+
+    if (item.completed) {
+      todoCompleted.append(li);
+    } else {
+      todoList.append(li);
+    }
+
+    li.querySelector('.todo-complete').addEventListener('click', function () {
+      item.completed = !item.completed;
+
+      render();
+    });
+
+    li.querySelector('.todo-remove').addEventListener('click', function () {
+      todoData.splice(itemIndex, 1);
+
+      render();
+    });
+  });
+};
+
+
+todoControl.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  if (headerInput.value.trim() !== "") {
+
+    let newToDo = {
+      text: headerInput.value,
+      completed: false
+    };
+
+    todoData.push(newToDo);
+    headerInput.value = '';
+
+    render();
+  }
+});
